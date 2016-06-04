830e436c7be302f8b96830a59898880468544e69
sherry97 <sherry97@gmail.com>
2016-06-04 18:31:49 -0400

    update

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+import bottle
 from pymongo import MongoClient
 import scrapy
 
@@ -7,6 +9,14 @@ class Worker():
         self.url = url
         self.skills = skills
 
+@bottle.get('/')
+def index():
+    return bottle.template('views/search.tpl')
+
+@bottle.post('/searched')
+def added():
+    parts = bottle.request.forms
+    return parts.getunicode("skills")
 
 def addToDB(db, name, url, skills, zipcode):
     result = db.workers.insert_one(
@@ -23,24 +33,32 @@ def searchDB(db, skills, zipcode, skillMatchThreshold):
 
     cursor = db.workers.find({"zip" : zipcode})
     for document in cursor:
-        skillMatch = len(list(set(skills) & set(document.skills)))//len(skills)
-        if (skillMatch >= skillMatchThreshold):
-            w.append(Worker(document.name, document.url, document.skills))
-
+        skillMatch = len(list(set(skills) & set(document['skills'])))/len(skills)
+        if skillMatch >= skillMatchThreshold:
+            try:
+                # print(document)
+                w.append(Worker(document['name'], document['URL'], document['skills']))
+            except KeyError:
+                continue
     return w
 
 def displayToForm(lst):
     for l in lst:
-        print("name: "+l.name+" || URL: "+l.url+" || skills: "+l.skills)
+        print(l.name+"\t"+l.url)
+        print(l.skills)
 
 def main():
+    bottle.debug(True)
+    bottle.run(host='0.0.0.0', port=8090)
+
     client = MongoClient()
     db = client.test
     # filler data
     addToDB(db, "Steve Rogers", "avengers.com/cap", ["shield throwing", "Java", "vigilante justice"], "20055")
     addToDB(db, "Natasha Romanov", "avengers.com/widow", ["assassination", "snark", "CSS"], "20055")
     addToDB(db, "Sam Wilson", "avengers.com/falcon", ["bird suit", "Java", "HTML", "CSS"], "20055")
-    skills = ["CSS", "HTML", "Java"]
+    # /filler data
+    skills = added()
     zipcode = "20055"
     skillMatchThreshold = 0.5
     matchedWorkers = searchDB(db, skills, zipcode, skillMatchThreshold)
@@ -49,6 +67,7 @@ def main():
         # add to DB
         pass
 
-    displayToForm(w);
+    displayToForm(matchedWorkers);
+    client.drop_database(db)
 
 if __name__ == '__main__': main()
